24952dc544925183673115aa96c2620b2760b57d
jdbbjd <1090336995@qq.com>
2017-09-07 16:27:10 +0800

    FUPLMSKitDemo init

---
+++ PLMediaStreamingKitDemo/PLMediaStreamingKitDemo/PLMediaStreamingKitDemo/PLMediaViewerViewController.h
@@ -0,0 +1,25 @@
+//
+//  PLMediaViewerViewController.h
+//  PLMediaStreamingKitDemo
+//
+//  Created by lawder on 16/6/30.
+//  Copyright © 2016年 NULL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+typedef NS_ENUM(NSInteger, PLMediaUserType) {
+    PLMediaUserTypeUnknown = 0,
+    PLMediaUserTypeSecondChief = 1,
+    PLMediaUserTypeViewer = 2
+};
+
+
+@interface PLMediaViewerViewController : UIViewController
+
+@property (nonatomic, assign) PLMediaUserType userType;
+@property (nonatomic, strong) NSString *roomName;
+@property (nonatomic, assign) BOOL audioOnly;
+
+@end
+
